b5b1e7fb2923dfac8cfcc71a9484ea9b119db9e1
Javier G. Sogo <jgsogo@gmail.com>
2019-01-02 11:15:25 +0100

    add tests for 'conan info'

--- conans/test/outputs/cli/test_info.py
+++ conans/test/outputs/cli/test_info.py
@@ -1,10 +1,10 @@
 # coding=utf-8
 
+import re
 import textwrap
 import unittest
 
-from parameterized import parameterized
-
+from conans.model.ref import ConanFileReference
 from conans.test.utils.tools import TestClient
 
 
@@ -13,21 +13,47 @@ class CLIInfoOutputTest(unittest.TestCase):
         from conans import ConanFile
 
         class Lib(ConanFile):
-            pass
+            {body}
         """)
 
     def setUp(self):
         self.client = TestClient()
-        files = {'conanfile.py': self.conanfile}
+        self.client.run("profile new --detect default")
+
+        p1 = "p1/version@user/channel"
+        files = {'conanfile.py': self.conanfile.format(body="pass")}
         self.client.save(files)
+        self.client.run("export . {}".format(p1))
+
+        p2 = "p2/version@user/channel"
+        files = {'conanfile.py': self.conanfile.format(body='requires = "{}"'.format(p1))}
+        self.client.save(files)
+        self.client.run("export . {}".format(p2))
 
         self.reference = "name/version@user/channel"
-        self.client.run("create . {}".format(self.reference))
+        files = {'conanfile.py': self.conanfile.format(body='requires = "{}"'.format(p2))}
+        self.client.save(files)
+        self.client.run("export . {}".format(self.reference))
 
-    @parameterized.expand([(True,), (False,)])
-    def test_get_file(self, use_raw):
-        raw = ' --raw' if use_raw else ''
+    def test_basic(self):
+        self.client.run("info {}".format(self.reference))
 
-        self.client.run("get {}{}".format(self.reference, raw))
-        output = str(self.client.out)[:-1]  # There is an extra '\n' in the output
-        self.assertEqual(output, self.conanfile)
+        # There are only a few types of lines:
+        for line in str(self.client.out).splitlines():
+            self.assertTrue(ConanFileReference.sep_pattern.match(line) or
+                            re.match("^\s{4}[\w\s]+:\s.+$", line) or
+                            line.lstrip() in ["Required by:", "Requires:"] or
+                            (line.startswith(" "*8) and ConanFileReference.sep_pattern.match(line.rstrip())))
+
+    def test_only(self):
+        self.client.run("info {} -n None".format(self.reference))
+        for line in str(self.client.out).splitlines():
+            self.assertTrue(ConanFileReference.sep_pattern.match(line))
+
+    def test_build_order(self):
+        self.client.run("info {} --build-order=p1/version@user/channel".format(self.reference))
+        list_refs = [it.strip('[] ') for it in str(self.client.out).split(',')]
+        refs = [it2.strip() for it in list_refs for it2 in it.split(',')]
+        self.assertTrue(len(refs) > 1)
+        for it in refs:
+            self.assertTrue(ConanFileReference.sep_pattern.match(it))
