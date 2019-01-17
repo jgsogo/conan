7933ac2630fcb8e1f4bd506df945dfc8199b4524
Javier G. Sogo <jgsogo@gmail.com>
2019-01-17 11:44:10 +0100

    add test with linked folder

--- conans/test/functional/tgz_md5_test.py
+++ conans/test/functional/tgz_md5_test.py
@@ -5,10 +5,11 @@ import time
 import unittest
 
 from conans.client.remote_manager import compress_files, uncompress_file
+from conans.model.manifest import gather_files
 from conans.paths import PACKAGE_TGZ_NAME
 from conans.test.utils.test_files import temp_folder
 from conans.test.utils.tools import TestBufferConanOutput
-from conans.util.files import save, md5sum
+from conans.util.files import save, md5sum, save_files
 
 
 class TgzMd5Test(unittest.TestCase):
@@ -51,7 +52,7 @@ class CompressSymlinksTest(unittest.TestCase):
             shutil.rmtree(self.tmp_folder)
 
     @unittest.skipIf(platform.system() == "Windows", "Command line 'tar' not available in Windows")
-    def test_symlinks_system(self):
+    def test_symlinks_file(self):
         file1 = os.path.join(self.tmp_folder, "src", "one_file.txt")
         symlink = os.path.join(self.tmp_folder, 'src', "symlink.txt")
 
@@ -75,3 +76,37 @@ class CompressSymlinksTest(unittest.TestCase):
         self.assertTrue(os.path.exists(os.path.join(self.tmp_folder, 'dest2', 'one_file.txt')))
         self.assertTrue(os.path.exists(os.path.join(self.tmp_folder, 'dest2', 'symlink.txt')))
         self.assertTrue(os.path.islink(os.path.join(self.tmp_folder, 'dest2', 'symlink.txt')))
+
+    @unittest.skipIf(platform.system() == "Windows", "Command line 'tar' not available in Windows")
+    def test_symlinks_directory(self):
+        # Prepare the folder with all the files
+        src_folder = os.path.join(self.tmp_folder, 'src')
+        save_files(src_folder, {'file.txt': "Conan",
+                                os.path.join('dir', 'file_dir.txt'): "Conan", })
+        os.symlink(os.path.join(src_folder, 'dir'), os.path.join(src_folder, 'symlink'))
+
+        # Create the zipped file
+        srcs, symlinks = gather_files(src_folder)
+        tar_gz_file = os.path.join(self.tmp_folder, PACKAGE_TGZ_NAME)
+        compress_files(files=srcs, symlinks=symlinks, name=tar_gz_file, dest_dir=self.tmp_folder)
+
+        # These will be the tests
+        def perform_tests(folder):
+            self.assertTrue(os.path.exists(os.path.join(folder, 'file.txt')))
+            self.assertTrue(os.path.exists(os.path.join(folder, 'dir', 'file_dir.txt')))
+            self.assertTrue(os.path.islink(os.path.join(folder, 'symlink')))
+            self.assertTrue(os.path.exists(os.path.join(folder, 'symlink', 'file_dir.txt')))
+
+        # Uncompress using CLI tools
+        dest_dir = os.path.join(self.tmp_folder, 'dest_cli')
+        os.makedirs(dest_dir)
+        os.system('tar xvzf "{}" -C "{}"'.format(tar_gz_file, dest_dir))
+
+        perform_tests(dest_dir)
+
+        # Uncompress using Conan machinery
+        dest_dir = os.path.join(self.tmp_folder, 'dest_conan')
+        uncompress_file(tar_gz_file, dest_dir, output=TestBufferConanOutput())
+
+        perform_tests(dest_dir)
+
