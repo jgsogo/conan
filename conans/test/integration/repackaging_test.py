fc61b310189c5eb009e3abe82378cb987818853c
Javier G. Sogo <jgsogo@gmail.com>
2019-03-20 13:59:23 +0100

    use textwrap

--- conans/test/integration/repackaging_test.py
+++ conans/test/integration/repackaging_test.py
@@ -1,7 +1,10 @@
 # coding=utf-8
 
+import textwrap
 import unittest
+
 from parameterized import parameterized
+
 from conans.test.utils.tools import TestClient, TestServer
 
 
@@ -11,14 +14,16 @@ class ReusingPackageTest(unittest.TestCase):
         self.client = TestClient(servers={"default": TestServer()},
                                  users={"default": [("lasote", "mypass")]})
 
-        conanfile = """from conans import ConanFile
-class BasePck(ConanFile):
-        name = "my_conanfile_base"
-        version = "1.0"
-
-        def build(self):
-            self.output.info(">>> BasePck '{}/{}'".format(self.name, self.version))
-"""
+        conanfile = textwrap.dedent("""
+            from conans import ConanFile
+            
+            class BasePck(ConanFile):
+                    name = "my_conanfile_base"
+                    version = "1.0"
+            
+                    def build(self):
+                        self.output.info(">>> BasePck '{}/{}'".format(self.name, self.version))
+            """)
         self.client.save({"conanfile.py": conanfile})
         self.client.run("create . lasote/testing")
         self.assertIn(">>> BasePck 'my_conanfile_base/1.0'", self.client.out)
@@ -26,16 +31,17 @@ class BasePck(ConanFile):
 
     @parameterized.expand([("package", ), ("my_conanfile_base", )])
     def test_build_requires(self, pck_name):
-        br = """from conans import ConanFile
+        br = textwrap.dedent("""
+            from conans import ConanFile
 
-class BrPck(ConanFile):
-    name = "%s"
-    version = "1.0.23"
-    build_requires = "my_conanfile_base/1.0@lasote/testing"
-
-    def build(self):
-        self.output.info(">>> BrPck '{}/{}'".format(self.name, self.version))
-""" % pck_name
+            class BrPck(ConanFile):
+                name = "%s"
+                version = "1.0.23"
+                build_requires = "my_conanfile_base/1.0@lasote/testing"
+            
+                def build(self):
+                    self.output.info(">>> BrPck '{}/{}'".format(self.name, self.version))
+            """ % pck_name)
         self.client.save({"conanfile.py": br}, clean_first=True)
         self.client.run("create . lasote/testing")
         self.assertIn(">>> BrPck '{}/1.0.23'".format(pck_name), self.client.out)
@@ -44,16 +50,17 @@ class BrPck(ConanFile):
                       "4fae0ab88f26e3a396351ac9 - Cache", self.client.out)
 
     def test_python_requires(self):
-        pr = """from conans import ConanFile, python_requires
+        pr = textwrap.dedent("""
+            from conans import ConanFile, python_requires
 
-base = python_requires("my_conanfile_base/1.0@lasote/testing")
-
-class PrPck(base.BasePck):
-    version = "pr"
-
-    def build(self):
-        self.output.info(">>> PrPck '{}/{}'".format(self.name, self.version))
-        """
+            base = python_requires("my_conanfile_base/1.0@lasote/testing")
+            
+            class PrPck(base.BasePck):
+                version = "pr"
+            
+                def build(self):
+                    self.output.info(">>> PrPck '{}/{}'".format(self.name, self.version))
+            """)
         self.client.save({"conanfile.py": pr}, clean_first=True)
         self.client.run("create . lasote/testing")
         self.assertIn(">>> PrPck 'my_conanfile_base/pr'", self.client.out)
@@ -62,18 +69,19 @@ class PrPck(base.BasePck):
                       "4fae0ab88f26e3a396351ac9 - Build", self.client.out)
 
     def test_both(self):
-        both = """from conans import ConanFile, python_requires
+        both = textwrap.dedent("""
+            from conans import ConanFile, python_requires
 
-requirement = "my_conanfile_base/1.0@lasote/testing"
-base = python_requires(requirement)
-
-class BothPck(base.BasePck):
-    version = "both"
-    build_requires = requirement
-
-    def build(self):
-        self.output.info(">>> BothPck '{}/{}'".format(self.name, self.version))
-        """
+            requirement = "my_conanfile_base/1.0@lasote/testing"
+            base = python_requires(requirement)
+            
+            class BothPck(base.BasePck):
+                version = "both"
+                build_requires = requirement
+            
+                def build(self):
+                    self.output.info(">>> BothPck '{}/{}'".format(self.name, self.version))
+            """)
         self.client.save({"conanfile.py": both}, clean_first=True)
         self.client.run("create . lasote/testing")
         self.assertIn(">>> BothPck 'my_conanfile_base/both'", self.client.out)
