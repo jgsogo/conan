ef02ba6a5e1d9d87fab36f2ffca79f40f47bafae
jgsogo <jgsogo@gmail.com>
2021-01-14 18:48:27 +0100

    use MesonToolchain

--- conans/test/integration/two_profiles/test_build_requires_env_propagation.py
+++ conans/test/integration/two_profiles/test_build_requires_env_propagation.py
@@ -20,20 +20,34 @@ class TestBuildRequiresEnvPropagation:
     consumer = textwrap.dedent("""
         import os
         from conans import ConanFile
+        from conan.tools.meson import Meson, MesonToolchain
 
         class Recipe(ConanFile):
-            build_requires = 'breq/version'
+            def generate(self):
+                tc = MesonToolchain(self)
+                tc.generate()
+
             def build(self):
                 for key, val in os.environ.items():
                     self.output.info(">> {}={}".format(key, val))
+    """)
+
+    profile_host = textwrap.dedent("""
+        include(default)
+        [build_requires]
+        breq/version
     """)
 
     def test_build_requires_environment(self):
         t = TestClient()
         t.save({
             'build_requires.py': self.build_requires,
-            'consumer.py': self.consumer
+            'consumer.py': self.consumer,
+            'profile_host': self.profile_host
         })
         t.run('create build_requires.py breq/version@ --profile=default')
-        t.run('create consumer.py consumer/version@ --profile:build=default --profile:host=default')
+        t.run('create consumer.py consumer/version@ --profile:build=default --profile:host=profile_host')
+        t.run(
+            'install consumer.py --profile:build=default --profile:host=profile_host')
+        # assert 'consumer/version: >> CC_LD=' in t.out
         print(t.out)
